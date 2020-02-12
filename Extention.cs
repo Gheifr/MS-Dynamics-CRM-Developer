f58db1350a215b09293d91081e76ac5c88518d9a
Gheifr <48731758+Gheifr@users.noreply.github.com>
2020-02-12 23:06:34 +0200

    add solution

---
+++ Extention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public static class Extention
+    {
+        public static bool IsNullOrEmpty(this Array array)
+        {
+            return (array == null || array.Length == 0);
+        }
+    }
+}
---
+++ WorkDayCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime output;
+
+            int weekendsCount = 0;
+
+
+            if (!weekEnds.IsNullOrEmpty())
+            {
+                foreach (WeekEnd item in weekEnds)
+                {
+                    if (!(startDate.AddDays(dayCount + weekendsCount) < item.EndDate))
+                    {
+                        weekendsCount += (item.EndDate - item.StartDate).Days;
+                    }
+                    else if (startDate.AddDays(dayCount + weekendsCount) == item.EndDate)
+                    {
+                        if (startDate.AddDays(dayCount + weekendsCount) < item.StartDate)
+                        {
+                            weekendsCount += (startDate.AddDays(dayCount + weekendsCount) - item.StartDate).Days;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return startDate.AddDays(dayCount-1);
+            }
+
+            output = startDate.AddDays(dayCount + weekendsCount);
+
+            return output;
+        }
+
+    }
+}
