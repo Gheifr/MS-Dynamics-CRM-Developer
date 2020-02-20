f5941dd2b90188b01b19e6c6265e122ba823511f
Gheifr <48731758+Gheifr@users.noreply.github.com>
2020-02-20 19:12:42 +0200

    removed useless stuff

--- WorkDayCalculator.cs
+++ WorkDayCalculator.cs
@@ -10,14 +10,13 @@ namespace CSharpTest
     {
         public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
         {
-            int weekEndDays =0;
-            List<DateTime> actualWeekEnds = new List<DateTime>();
 
             if (dayCount < 0)
             {
                 throw new WorkDayCalculatorException("Days count is less than 0!");
             }
 
+            List<DateTime> actualWeekEnds = new List<DateTime>();
             DateTime estimatedEndDate = startDate.AddDays(dayCount == 0 ? 0 : dayCount - 1);
 
             if (weekEnds.IsNullOrEmpty())
@@ -32,55 +31,17 @@ namespace CSharpTest
                     throw new WorkDayCalculatorException("Weekend period end date is less than it's start day!");
                 }
 
-                if (item.EndDate < startDate)
+                if (item.StartDate != item.EndDate)
                 {
-                    continue;
-                }
-
-                if (item.StartDate > estimatedEndDate)
-                {
-                    continue;
-                }
-                
-                if (item.StartDate <= startDate && item.EndDate >= startDate)
-                {
-                    //estimatedEndDate = estimatedEndDate.AddDays(item.EndDate.Day - startDate.Day + 1);
-                    //continue;
-                    if (item.StartDate != item.EndDate)
+                    for (int i = 0; i <= item.EndDate.Day - startDate.Day; i++)
                     {
-                        for (int i = 0; i <= item.EndDate.Day - startDate.Day; i++)
-                        {
-                            actualWeekEnds.Add(startDate.AddDays(i));
-                        }
-                    }
-                    else
-                    {
-                        actualWeekEnds.Add(startDate);
-                    }
-                    continue;
-                }
-
-                if (item.StartDate >= startDate && item.StartDate <= estimatedEndDate)
-                {
-                    //estimatedEndDate = estimatedEndDate.AddDays(item.EndDate.Day - item.StartDate.Day + 1);
-                    if (item.StartDate != item.EndDate)
-                    {
-                        for (int i = 0; i <= item.EndDate.Day - item.StartDate.Day; i++)
-                        {
-                            actualWeekEnds.Add(item.StartDate.AddDays(i));
-                        }
-                    }
-                    else
-                    {
-                        actualWeekEnds.Add(item.StartDate);
+                        actualWeekEnds.Add(startDate.AddDays(i));
                     }
                 }
-
-                actualWeekEnds = actualWeekEnds.Distinct().ToList();
-
-                weekEndDays = actualWeekEnds.Count - weekEndDays;
-
-                estimatedEndDate.AddDays(weekEndDays);
+                else
+                {
+                    actualWeekEnds.Add(startDate);
+                }
             }
 
             
